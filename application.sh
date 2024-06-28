ba1073e596d0798d2dd3cd912297142d6749e15d
warithz <engineerwarithz@gmail.com>
2024-06-28 07:14:34 -0400

    welcome to our new application

---
+++ application.sh
@@ -0,0 +1,13 @@
+$name = warithz
+echo "Before opening application, input your name..."
+echo "welcome engineer $name"
+
+echo "now opening the application modules..."
+echo "checking for malwares.... DONE"
+
+line 1 
+Addition: A + B + C = D
+line 2
+Subtraction: A - B - C = D
+line 3
+Multiplication: A x B x C =D
